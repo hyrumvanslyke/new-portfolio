7818740e8fd3eedfc4c2ba7687c8ff1600c60740
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2024-01-18 14:41:02 -0500

    Navbar work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import Navbar from "./components/NavBar/navbar";
 function App() {
   return (
     <div className="App">
-     
+     <Navbar></Navbar>
     </div>
   );
 }
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <nav className="navbar">
+        <img src={logo} alt="logo" className='logo'/>
+        <div className="desktopMenu"></div>
+        <buttton className="desktopMenu">
+            <img src="" alt="" className="destopMenuImg" />Contact Me
+        </buttton>
+    </nav>
   )
 }
 
